04f08895cb81b27297d442134b39d75df00c6b1d
Ikto <lorenzolimatola@gmail.com>
2024-05-14 22:48:09 +0200

    Rimosso lo sfondo alle immagini, aggiunta la mezz'ora di pausa pranzo obbligatoria aggiunti confetti

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function showRandomPopup() {
-  // Genera un numero casuale tra 0 e 1
-  var random = Math.random();
-  // Mostra il pop-up solo se il numero casuale è inferiore a 0.5 (50% di probabilità)
-  if (random < 0.5) {
-    var response = confirm("Sei sicuro di voler uscire?? DAI RESTA ANCORA!!!");
-    if (!response) {
-      alert("Bravo! RESTA ANCORA QUI CON NOI");
-      return false;
-    }
-  }
-}
-
 function calculateTime() {
   // Mostra il pop-up casuale
   //showRandomPopup();
 
-   var currentTime = new Date(); // Ottieni l'orario attuale
-  
+  var currentTime = new Date(); // Ottieni l'orario attuale
+
   var inputTime = document.getElementById('inputTime').value;
   var additionalTime = document.getElementById('additionalTime').value;
 
@@ -26,25 +13,40 @@ function calculateTime() {
   var additionalMinutes = parseInt(additionalTime.substring(3));
 
   time.setHours(time.getHours() + additionalHours);
-  time.setMinutes(time.getMinutes() + additionalMinutes);
+  time.setMinutes(time.getMinutes() + additionalMinutes + 30);
 
   var hours = String(time.getHours()).padStart(2, '0');
   var minutes = String(time.getMinutes()).padStart(2, '0');
   var resultTime = hours + ":" + minutes;
 
   var resultDisplay = document.getElementById('result');
-  
-  // Nascondi l'immagine pre-bottone e mostra l'immagine post-bottone
+
   document.getElementById('preButtonImage').style.display = 'none';
-  
+
   if (resultTime > currentTime.toLocaleTimeString()) {
     document.getElementById('result').innerText = "Troppo presto! Devi aspettare le " + resultTime;
-	document.getElementById('postButtonImage').style.display = 'none';
-	document.getElementById('thirdImage').style.display = 'inline';
-	
-    } else {
-		document.getElementById('result').innerText = "Fantastico potevi marcare alle " + resultTime;
-		document.getElementById('thirdImage').style.display = 'none';
-		document.getElementById('postButtonImage').style.display = 'inline';
+    document.getElementById('postButtonImage').style.display = 'none';
+    document.getElementById('thirdImage').style.display = 'inline';
+  } else {
+    document.getElementById('result').innerText = "Fantastico potevi marcare alle " + resultTime;
+    document.getElementById('thirdImage').style.display = 'none';
+    document.getElementById('postButtonImage').style.display = 'inline';
+
+    for (var i = 0; i < 20; i++) {
+      setTimeout(function () {
+        const confettiElement = document.createElement('div');
+        confettiElement.classList.add('confetti');
+        document.body.appendChild(confettiElement);
+
+        const randomX = Math.random() * window.innerWidth;
+        const randomY = Math.random() * window.innerHeight + -500;
+        confettiElement.style.left = randomX + 'px';
+        confettiElement.style.top = randomY + 'px';
+
+        setTimeout(function () {
+          confettiElement.remove();
+        }, 3000);
+      }, i * 100);
     }
+  }
 }
